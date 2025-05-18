084cc48381432892ee02f4702b70534bcaea6640
Shresta Agrawal <shrestaagrawal400@gmail.com>
2025-05-18 16:55:35 +0530

    git push origin main

--- Java/sequential_search/prg8.java
+++ Java/sequential_search/prg8.java
@@ -13,7 +13,8 @@ public class prg8 {
         boolean found = false;
             // Check if the key is present in the array
         // If the key is found, set found to true
-        for (int j = 0; j < Ar.length; j++) {
+        for (int j = 0; j < Ar.length; j++) 
+        {
             if (Ar[j] == key) {
                 found = true;
                 break;
