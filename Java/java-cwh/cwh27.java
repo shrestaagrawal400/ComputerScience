2b817a30fc5b50b5e8aa2fc503b39a196085e817
Shresta Agrawal <shrestaagrawal400@gmail.com>
2025-05-28 08:47:56 +0530

    efn;

--- Java/java-cwh/cwh27.java
+++ Java/java-cwh/cwh27.java
@@ -1,3 +1,9 @@
 public class cwh27 {
-    
+ public static void main(String[] args) {
+  int [] [] flats;
+  flats=new int[2][3];
+  flats[0][0]=101;
+    flats[0][1]=102;
+    flats[0][2]=103;
+}   
 }
