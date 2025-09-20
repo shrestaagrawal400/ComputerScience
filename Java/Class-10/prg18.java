8e8a6ef248390576bb09daf9eee54733b168e044
Shresta Agrawal <shrestaagrawal400@gmail.com>
2025-09-20 14:36:43 +0530

    *

--- Java/Class-10/prg18.java
+++ Java/Class-10/prg18.java
@@ -29,5 +29,25 @@ public class prg18
                 System.out.println("Record does not exist");
             }
                 System.out.println("End of program");
-            }
-        }
+       //sorting
+         int temp;
+          for(int i=0; i<Ar.length; i++)
+          {
+                for(int j=i+1; j<Ar.length; j++)
+                {
+                 if(Ar[i]>Ar[j])
+                 {
+                      temp = Ar[i];
+                      Ar[i] = Ar[j];
+                      Ar[j] = temp;
+                 }
+                }
+          }
+          System.out.println("Sorted array:");
+          for(int i=0; i<Ar.length; i++)
+          {
+                System.out.println(Ar[i]);
+          }
+          sc.close();
+          }}
+          
